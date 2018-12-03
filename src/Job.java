c808d3c7395f39c1adef2b05b273fe84afbbab2e
marco-gerardi <marco.gerardi@gmail.com>
2018-12-03 22:39:30 +0100

    modificata classe job

--- src/Job.java
+++ src/Job.java
@@ -1,31 +1,38 @@
 public class Job {
 
 	//dichiarazione variabile tempo di servizio e variabile indicante il momento della creazione del job
+	private int id; 
+
 	private double processingTime; 
 	private double startServiceTime; 
 
 	//Costruttori della classe
-	public Job(double processingTime, double startServiceTime, double endServiceTime)
+	public Job(int id, double processingTime, double startServiceTime)
 	{
+		this.id=id;
 		this.processingTime = processingTime;
 		this.startServiceTime = startServiceTime;
 	}
 
-	public Job(double processingTime, double startServiceTime)
+	public Job(int id, double processingTime)
 	{
-		this(processingTime, startServiceTime, startServiceTime);
+		this(id,processingTime, 0);
 	}
 
-	public Job()
+	public Job(int id)
 	{
-		this(0, 0, 0);
+		this(id,0, 0);
 	}
 
 	//metodi get che restituiscono i valori delle variabili della classe
 	public double getProcessingTime() { return processingTime; }
 	public double getStartServiceTime() { return startServiceTime; }
+	public int getId() { return id; }
+
+
 
 	//metodi set che impostano i valori delle variabili della classe
 	public void setProcessingTime(double processingTime) {this.processingTime = processingTime;}
 	public void setStartServiceTime(double startServiceTime) {this.startServiceTime = startServiceTime;}
+	public void setId(int id) { this.id = id; }
 }
